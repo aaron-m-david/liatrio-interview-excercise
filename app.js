6ae69a88a5f83304d09ced85ee1bf99c5d62b7ca
AaronDavid <amdavid@mail.csuchico.edu>
2023-10-30 13:31:07 -0700

    add clarifying comments

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// Load express module
 const express = require('express')
+// Create instance of express application
 const app = express()
+// App will listen onto port 3000
 const port = 3000
+// Send JSON response with message and timestamp
 app.get('/', (req, res) => {
     res.json({"message": "My name is Aaron",
               "timestamp": Date.now()})
